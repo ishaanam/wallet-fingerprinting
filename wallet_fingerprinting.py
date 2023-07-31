9f19810ef77782594a2c92f28644ff0de364d474
ishaanam <ishaana.misra@gmail.com>
2023-07-30 21:30:11 -0400

    Add initial code for interacting with Bitcoin Core

---
+++ wallet_fingerprinting.py
@@ -0,0 +1,45 @@
+import configparser
+import json
+import requests
+
+Config = configparser.ConfigParser()
+Config.read("rpc_config.ini")
+
+URL = Config.get("RPC_INFO", "URL")
+RPCUSER = Config.get("RPC_INFO", "RPCUSER")
+RPCPASSWORD = Config.get("RPC_INFO", "RPCPASSWORD")
+
+def getbestblockhash():
+    payload = json.dumps({"method": "getbestblockhash", "params": []})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+    response = requests.request("POST", URL, data=payload, headers=headers, auth=(RPCUSER, RPCPASSWORD))
+
+    return json.loads(response.text)["result"]
+
+def getblock(blockhash):
+    payload = json.dumps({"method": "getblock", "params": [block_hash]})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+    response = requests.request("POST", URL, data=payload, headers=headers, auth=(RPCUSER, RPCPASSWORD))
+
+    return json.loads(response.text)["result"]
+
+def getrawmempool():
+    payload = json.dumps({"method": "getrawmempool", "params": []})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+    response = requests.request("POST", URL, data=payload, headers=headers, auth=(RPCUSER, RPCPASSWORD))
+
+    return json.loads(response.text)["result"]
+
+def getrawtransaction(txid):
+    payload = json.dumps({"method": "getrawtransaction", "params": [txid]})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+    response = requests.request("POST", URL, data=payload, headers=headers, auth=(RPCUSER, RPCPASSWORD))
+
+    return json.loads(response.text)["result"]
+
+def decoderawtransaction(tx_hex):
+    payload = json.dumps({"method": "decoderawtransaction", "params": [tx_hex]})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+    response = requests.request("POST", URL, data=payload, headers=headers, auth=(RPCUSER, RPCPASSWORD))
+
+    return json.loads(response.text)["result"]
